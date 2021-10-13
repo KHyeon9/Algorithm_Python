5a810cd7e1228d5cb5efe7abbbe702cfda310c18
KHyeon9 <92162623+KHyeon9@users.noreply.github.com>
2021-10-13 18:04:17 +0900

    심부름 가는 길
    BOJ 5554: 심부름 가는 길

---
+++ BOJ/Bronze/5554.py
@@ -0,0 +1,7 @@
+s = 0                       
+for i in range(0, 4):       
+    a = input()             
+    s += int(a)             
+                            
+print(s // 60)              
+print(s % 60)  

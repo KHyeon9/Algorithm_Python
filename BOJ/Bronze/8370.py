2eeb83d41fb4b7db4cebc2dd696f082aabdaa241
KHyeon9 <92162623+KHyeon9@users.noreply.github.com>
2021-10-13 18:07:38 +0900

    Plane
    BOJ 8370: Plane

---
+++ BOJ/Bronze/8370.py
@@ -0,0 +1,6 @@
+a = list(map(int, input().split()))
+                                   
+mul = a[0] * a[1]                  
+mul2 = a[2] * a[3]                 
+                                   
+print(mul + mul2)                  

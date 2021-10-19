12b589a71a0d15bec8a0e7a1c078a1bdd2bf66aa
KHyeon9 <92162623+KHyeon9@users.noreply.github.com>
2021-10-19 14:37:07 +0900

    삼각형 외우기
    BOJ 10101: 삼각형 외우기

---
+++ BOJ/Bronze/10101.py
@@ -0,0 +1,16 @@
+a = int(input())
+b = int(input())
+c = int(input())
+
+if a == b == c:
+    print("Equilateral")
+
+elif a + b + c == 180:
+    if a == b or a == c or b == c:
+        print("Isosceles")
+
+    else:
+        print("Scalene")
+
+elif a + b + c != 180:
+    print("Error")

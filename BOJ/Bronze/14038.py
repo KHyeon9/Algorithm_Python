abefa7b1ec0945946583ac08c1f5a782d8baf0b7
KHyeon9 <92162623+KHyeon9@users.noreply.github.com>
2021-10-21 18:20:21 +0900

    Tournament Selection
    BOJ 14038: Tournament Selection

---
+++ BOJ/Bronze/14038.py
@@ -0,0 +1,19 @@
+w = 0
+
+for i in range(6):
+    r = input()
+
+    if r == 'W':
+        w += 1
+
+if w == 5 or w == 6 :
+    print("1")
+
+elif w == 3 or w == 4:
+    print("2")
+
+elif w == 1 or w == 2:
+    print("3")
+
+else:
+    print("-1")

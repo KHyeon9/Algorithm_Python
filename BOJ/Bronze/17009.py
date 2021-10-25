fbe586128c7ddb2f219c7b14602e76a7b28681f1
KHyeon9 <92162623+KHyeon9@users.noreply.github.com>
2021-10-25 18:43:43 +0900

    Winning Score
    BOJ 17009: Winning Score

---
+++ BOJ/Bronze/17009.py
@@ -0,0 +1,18 @@
+a1 = int(input())
+a2 = int(input())
+a3 = int(input())
+b1 = int(input())
+b2 = int(input())
+b3 = int(input())
+
+A = a1 * 3 + a2 * 2 + a3
+B = b1 * 3 + b2 * 2 + b3
+
+if A > B:
+    print("A")
+
+elif A < B:
+    print("B")
+
+else:
+    print("T")

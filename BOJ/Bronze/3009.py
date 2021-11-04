eb7a4f5e6b9d72a4d77b1fabf457d2661a8b6b5f
KHyeon9 <92162623+KHyeon9@users.noreply.github.com>
2021-11-04 18:09:10 +0900

    네 번째 점
    BOJ 3009: 네 번째 점

---
+++ BOJ/Bronze/3009.py
@@ -0,0 +1,23 @@
+x1, y1 = list(map(int, input().split()))
+x2, y2 = list(map(int, input().split()))
+x3, y3 = list(map(int, input().split()))
+
+if x1 == x2:
+    x = x3
+
+elif x1 == x3:
+    x = x2
+
+else:
+    x = x1
+
+if y1 == y2:
+    y = y3
+
+elif y1 == y3:
+    y = y2
+
+else:
+    y = y1
+
+print(x, y)

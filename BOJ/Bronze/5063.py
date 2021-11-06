4cfc20532219700962199dfb99bbf2cd65ee4eef
KHyeon9 <92162623+KHyeon9@users.noreply.github.com>
2021-11-06 15:27:02 +0900

    TGN
    BOJ 5063: TGN

---
+++ BOJ/Bronze/5063.py
@@ -0,0 +1,13 @@
+t = int(input())
+
+for i in range(t):
+    r, e, c = list(map(int, input().split()))
+
+    if r < e - c:
+        print("advertise")
+
+    elif r > e - c:
+        print("do not advertise")
+
+    else:
+        print("does not matter")

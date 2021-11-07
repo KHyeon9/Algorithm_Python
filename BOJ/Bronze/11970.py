4ccc916e8f91514995efe770bdb07b4cf1442f3a
KHyeon9 <92162623+KHyeon9@users.noreply.github.com>
2021-11-07 13:45:55 +0900

    Fence Painting
    BOJ 11970: Fence Painting

---
+++ BOJ/Bronze/11970.py
@@ -0,0 +1,8 @@
+a, b = map(int, input().split())
+c, d = map(int, input().split())
+arr = [0] * 101
+for i in range(a, b):
+    arr[i] = 1
+for i in range(c, d):
+    arr[i] = 1
+print(arr.count(1))

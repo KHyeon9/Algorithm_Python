b700576ad90090642ad388b93c86a56532361bdd
KHyeon9 <92162623+KHyeon9@users.noreply.github.com>
2021-11-26 20:03:58 +0900

    직사각형을 만드는 방법
    BOJ 8320: 직사각형을 만드는 방법

---
+++ BOJ/Bronze/8320.py
@@ -0,0 +1,13 @@
+from sys import stdin
+n = int(stdin.readline())
+r = 0
+
+for i in range(1, n + 1):
+    for j in range(i, n + 1):
+        if i * j <= n:
+            r += 1
+
+        else:
+            break
+
+print(r)

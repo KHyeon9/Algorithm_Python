54e725478cca285e2da34d8a7488107d76fc1f17
KHyeon9 <92162623+KHyeon9@users.noreply.github.com>
2021-11-28 16:59:53 +0900

    이진수 합
    BOJ 8741: 이진수 합

--- BOJ/Bronze/8714.py
+++ BOJ/Bronze/8714.py
@@ -1,11 +1,3 @@
 n = int(input())
-a = list(map(int, input().split()))
-r = 0
-for i in range(n):
-    if a[i] == 1:
-        r += 1
-
-if n - r >= r:
-    print(r)
-else:
-    print(n - r)
+num = 2 ** n
+print(bin((num * (num - 1)) // 2)[2:])

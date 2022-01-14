e11c77c4a663659a8e6ae2bcee6a5e8a36b29be4
KHyeon9 <92162623+KHyeon9@users.noreply.github.com>
2022-01-14 17:47:07 +0900

    최소 힙
    BOJ 1927:  최소 힙

---
+++ BOJ/Silver/1927.py
@@ -0,0 +1,12 @@
+import heapq
+from sys import stdin
+heap = []
+for _ in range(int(stdin.readline())):
+    n = int(stdin.readline())
+    if n == 0:
+        if len(heap) == 0:
+            print(0)
+        else:
+            print(heapq.heappop(heap))
+    else:
+        heapq.heappush(heap, n)

6fe225cd53d879ce1e649530b21fbed2ebced902
KHyeon9 <92162623+KHyeon9@users.noreply.github.com>
2022-01-19 17:45:14 +0900

    Z
    BOJ 1074: Z

---
+++ BOJ/Silver/1074.py
@@ -0,0 +1,17 @@
+N, r, c = map(int, input().split())
+result = 0
+while N != 0:
+    N -= 1
+    if r < 2 ** N and c < 2 ** N:
+        result += 0
+    elif r < 2 ** N <= c:
+        result += (2 ** N) * (2 ** N)
+        c -= 2 ** N
+    elif c < 2 ** N <= r:
+        result += (2 ** N) * (2 ** N) * 2
+        r -= 2 ** N
+    else:
+        result += (2 ** N) * (2 ** N) * 3
+        c -= 2 ** N
+        r -= 2 ** N
+print(result)

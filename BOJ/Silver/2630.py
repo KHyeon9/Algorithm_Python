efdfdbf5be487a6ece386dbccc0d6da09183831b
KHyeon9 <92162623+KHyeon9@users.noreply.github.com>
2022-01-25 17:32:39 +0900

    쿼드트리
    BOJ 2630: 쿼드트리

--- BOJ/Silver/2630.py
+++ BOJ/Silver/2630.py
@@ -3,21 +3,19 @@ def sol(x, y, num):
     for i in range(x, x + num):
         for j in range(y, y + num):
             if color != arr[i][j]:
+                print('(', end='')
                 sol(x, y, num // 2)
                 sol(x, y + num // 2, num // 2)
                 sol(x + num // 2, y, num // 2)
                 sol(x + num // 2, y + num // 2, num // 2)
+                print(')', end='')
                 return
-    if color == 0:
-        result[0] += 1
+    if color == 1:
+        print(1, end='')
     else:
-        result[1] += 1
+        print(0, end='')
 
 
 n = int(input())
-arr = [list(map(int, input().split())) for _ in range(n)]
-result = [0, 0]
+arr = [list(map(int, input())) for _ in range(n)]
 sol(0, 0, n)
-print(result[0])
-print(result[1])
-

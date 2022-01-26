3b3e882619da2833bd190c409af7f863032e1e87
KHyeon9 <92162623+KHyeon9@users.noreply.github.com>
2022-01-26 18:16:14 +0900

    경로 찾기
    BOJ 11403: 경로 찾기

---
+++ BOJ/Silver/11403.py
@@ -0,0 +1,9 @@
+n = int(input())
+arr = [list(map(int, input().split())) for _ in range(n)]
+for i in range(n):
+    for j in range(n):
+        for k in range(n):
+            if arr[j][i] == 1 and arr[i][k] == 1:
+                arr[j][k] = 1
+for i in arr:
+    print(*i)

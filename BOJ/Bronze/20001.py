be81723a18e994be1c05f7e5f1c4785ffd660566
KHyeon9 <92162623+KHyeon9@users.noreply.github.com>
2022-03-15 16:23:11 +0900

    고무오리 디버깅
    BOJ 20001: 고무오리 디버깅

---
+++ BOJ/Bronze/20001.py
@@ -0,0 +1,14 @@
+start = input()
+cnt = 0
+while 1:
+    s = input()
+    if s == '문제':
+        cnt += 1
+    elif s == '고무오리':
+        if cnt == 0:
+            cnt += 2
+        else:
+            cnt -= 1
+    else:
+        break
+print('고무오리야 사랑해' if cnt == 0 else '힝구')

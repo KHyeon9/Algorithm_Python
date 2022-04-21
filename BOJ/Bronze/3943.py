d46ca32052012a25976beb4675a6bcb251ee8ce1
KHyeon9 <92162623+KHyeon9@users.noreply.github.com>
2022-04-21 15:06:38 +0900

    헤일스톤 수열
    BOJ 3943: 헤일스톤 수열

---
+++ BOJ/Bronze/3943.py
@@ -0,0 +1,13 @@
+from sys import stdin
+
+for _ in range(int(input())):
+    n = int(stdin.readline())
+    arr = [n]
+
+    while n != 1:
+        if n % 2 == 0:
+            n //= 2
+        else:
+            n = n * 3 + 1
+        arr.append(n)
+    print(max(arr))

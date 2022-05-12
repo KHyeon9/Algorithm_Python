854dc9523dd93baad1afdc75c92826ef094a9ab5
KHyeon9 <92162623+KHyeon9@users.noreply.github.com>
2022-05-12 17:52:42 +0900

    Maximum Subarray
    BOJ 10211: Maximum Subarray

---
+++ BOJ/Silver/10211.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+for _ in range(int(input())):
+    n = int(input())
+    nums = list(map(int, input().split()))
+    dp = [nums[0]]
+    for i in range(1, n):
+        dp.append(max(dp[i - 1] + nums[i], nums[i]))
+
+    print(max(dp))

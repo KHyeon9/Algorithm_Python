d848fb8ea351f627b5f49012cdbbe00671f24f49
KHyeon9 <92162623+KHyeon9@users.noreply.github.com>
2023-02-13 14:15:11 +0900

    Fold the Paper Nicely
    BOJ 26340: Fold the Paper Nicely

---
+++ BOJ/Bronze/26340.py
@@ -0,0 +1,12 @@
+for _ in range(int(input())):
+    a, b, c = map(int, input().split())
+    print(f"Data set: {a} {b} {c}")
+
+    for _ in range(c):
+        if a > b:
+            a //= 2
+        else:
+            b //= 2
+
+    print(max(a, b), min(a, b))
+    print()

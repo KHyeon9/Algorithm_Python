cd8cd82737dda97c519a2b6148a8c71307bbf05b
KHyeon9 <92162623+KHyeon9@users.noreply.github.com>
2023-04-19 16:07:53 +0900

    특별한 학교 이름
    BOJ 27889: 특별한 학교 이름

---
+++ BOJ/Bronze/27889.py
@@ -0,0 +1,8 @@
+name = {
+        'NLCS':'North London Collegiate School',
+        'BHA': 'Branksome Hall Asia',
+        'KIS': 'Korea International School',
+        'SJA': 'St. Johnsbury Academy'
+    }
+
+print(name[input()])

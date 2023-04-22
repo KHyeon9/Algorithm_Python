f4a607564868a32a787d500a369b77758bd9fbaf
KHyeon9 <92162623+KHyeon9@users.noreply.github.com>
2023-04-23 03:38:26 +0900

    STROJOPIS
    BOJ 10551: STROJOPIS

---
+++ BOJ/Bronze/10551.py
@@ -0,0 +1,22 @@
+s = input()
+res = [0] * 8
+
+for i in s:
+    if i in '1QAZ':
+        res[0] += 1
+    elif i in '2WSX':
+        res[1] += 1
+    elif i in '3EDC':
+        res[2] += 1
+    elif i in '4RFV5TGB':
+        res[3] += 1
+    elif i in '6YHN7UJM':
+        res[4] += 1
+    elif i in '8IK,':
+        res[5] += 1
+    elif i in '9OL.':
+        res[6] += 1
+    else:
+        res[7] += 1
+for i in res:
+    print(i)

98bcd410e0a26ad68c9a2f22462120587d4e034d
KHyeon9 <92162623+KHyeon9@users.noreply.github.com>
2023-05-18 14:04:23 +0900

    ACM 대회 채점
    BOJ 11531: ACM 대회 채점

--- BOJ/Bronze/11531.py
+++ BOJ/Bronze/11531.py
@@ -1,3 +1,25 @@
-n = int(input())
-s = input()
-print(s[-5:])
+dic = {}
+cnt = 0
+penalty = 0
+
+while True:
+    problem = list(input().split())
+
+    if problem[0] == '-1':
+        break
+
+    if problem[2] == "right":
+        cnt += 1
+        penalty += int(problem[0])
+        if problem[1] in dic:
+            penalty += dic[problem[1]] * 20
+        continue
+
+    if problem[1] not in dic:
+        dic[problem[1]] = 1
+    else:
+        dic[problem[1]] += 1
+
+print(cnt, penalty)
+
+
